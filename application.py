7d40165a7230d4b67b87e19e1ff30884a7736f6e
Ross Guy <RossG2@discovery.co.za>
2020-03-29 00:05:46 +0200

    added crazy fancy menu feature

--- application.py
+++ application.py
@@ -19,11 +19,16 @@ db = SQLAlchemy(application)
 
 SETTINGS_ENABLE_SMS = True
 NEW_LINE = '\r\n'
+API_BASE_PATH = 'https://c640a319.ngrok.io'
+DEST_TYPE_MENU = "MENU"
+DEST_TYPE_TASK = "TASK"
 
 # Twilio Dinges make these environment vars
 account = "TwilioAccountID"
 token = "TwilioAccountToken"
 client = Client(account, token)
+
+
 # End Twilio Dinges
 
 
@@ -316,21 +321,127 @@ def add_bank_details():
     })
     return response, 201
 
-# https://c640a319.ngrok.io/api/v1/coronials/init
+
+menus = {}
+
+### TEAM INFO around how to use menu system
+# -> Main Menu is defined at the root as menus['main'] see below
+# -> To add a new menu, create menus['menu-name'] = {} use menus['main'] or menus['business'] as example
+# -> To link to a menu use DEST_TYPE_MENU type and set value to menu as defined here. EG menus['business']
+# -> To link to a task defined in twilio, use DEST_TYPE_TASK type and set value to task as defined in twilio
+# -> remember to create call-back function..
+
+menus['main'] = {
+    'intro': "I'm TheCoronials bot for COVID-19 Hackathon!",
+    'options': [
+        {
+            'friendly': 'My Profile',
+            'dest': {
+                'type': DEST_TYPE_TASK,
+                'value': 'profile_menu'
+            },
+        },
+        {
+            'friendly': 'Social Grants',
+            'dest': {
+                'type': DEST_TYPE_TASK,
+                'value': 'profile_menu'
+            },
+        },
+        {
+            'friendly': 'Business Menu',
+            'dest': {
+                'type': DEST_TYPE_MENU,
+                'value': 'business'
+            },
+        },
+    ]
+}
+
+menus['business'] = {
+    'intro': "This is the Business Menu",
+    'options': [
+        {
+            'friendly': 'Business Loans',
+            'dest': {
+                'type': DEST_TYPE_TASK,
+                'value': 'business_loans'
+            },
+        },
+        {
+            'friendly': 'Global Back',
+            'dest': {
+                'type': DEST_TYPE_TASK,
+                'value': 'global_back'
+            },
+        },
+    ]
+}
+
+
+def determine_menu_redirect(menu, selection):
+    dest = get_dest_for_selection(menu, selection)
+    if dest['type'] == DEST_TYPE_TASK:
+        return build_twilio_task_redirect(dest['value'])
+
+# TODO get name here
+    if dest['type'] == DEST_TYPE_MENU:
+        return build_twilio_collect_from_menu(dest['value'], 'BOB')
+
+
+def get_dest_for_selection(menu, selection):
+    index = int(selection) - 1
+    return menus[menu]['options'][index]['dest']
+
+
+def get_menu(menu, user_name):
+    response = "Heyyy {},\n".format(user_name)
+    response += menus[menu]['intro'] + "\n"
+    for i, item in enumerate(menus[menu]['options'], start=1):
+        response += "{}) {}\n".format(str(i), item['friendly'])
+
+    response += '\n\n'
+
+    return response
+
 
 @application.route('/api/v1/coronials/init', methods=['GET', 'POST'])
 def get_init():
     payload = request.form
     userId = payload['UserIdentifier']
     print('user init ' + userId)
+    # print('PROFILE? ' + menus['main']['options'][1])
 
     try:
         user = get_user_by_user_identifier(userId)
-        response = "Hello {}, I'm TheCoronials bot for COVID-19 Hackathon\n1) menu\n2) options\n3) profile menue".format(user.name)
-        return build_twilio_say(response)
+        return build_twilio_collect_from_menu('main', user.name)
     except NoResultFound:
         print('Not registered yet...')
         return build_twilio_task_redirect('register')
+
+
+@application.route('/api/v1/menu/main-callback', methods=['GET', 'POST'])
+def callback_main():
+    # TODO may break if they don't put 1 as answer.. handle..
+    selection = int(get_menu_response(request))
+    response = determine_menu_redirect('main', selection)
+    return response
+
+
+@application.route('/api/v1/menu/business-callback', methods=['GET', 'POST'])
+def callback_business():
+    # TODO may break if they don't put 1 as answer.. handle..
+    selection = int(get_menu_response(request))
+    response = determine_menu_redirect('business', selection)
+    return response
+
+
+def get_menu_response(request):
+    payload = request.form
+    memory = json.loads(payload['Memory'])
+    answers = memory['twilio']['collected_data']['collect_menu_selection']['answers']
+    selection = answers['menu_selection']['answer']
+    return selection
 
 
 @application.route('/api/v1/coronials/create-user', methods=['GET', 'POST'])
@@ -362,8 +473,9 @@ def create_user_twilio():
 
 @application.route('/api/v1/coronials/hello', methods=['GET', 'POST'])
 def get_hello():
-    response = "Hello, this is the backend on AWS saying WORLD"
-    return build_twilio_say(response)
+    question = "Hello, this is the backend on AWS saying WORLD"
+    callback = "sdome url goes here WORLD"
+    return build_twilio_collect(question, callback)
 
 
 @application.route('/api/v1/coronials/image', methods=['GET', 'POST'])
@@ -382,6 +494,7 @@ def get_age():
     response = "Double your age is {}".format(age)
 
     return build_twilio_say(response)
+
 
 @application.route('/api/v1/deployer/labels', methods=['GET', 'POST'])
 def get_labels():
@@ -431,7 +544,6 @@ def do_deploy_schedule():
                                '16:00',
                                '16h00',
                                '4pm']:
-
         result = "Please deploy during the allotted time slots!\n\n" \
                  "09h00 until 09h30\n" \
                  "12h00 until 12h30\n" \
@@ -447,14 +559,67 @@ def send_sms(body):
         print(">>>>SMS<<<<\n{}\n>>>>>SMS<<<<<<".format(body))
 
 
+def build_twilio_collect_from_menu(menu, user_name):
+    menu_response = get_menu(menu, user_name)
+    redirect_path = get_full_api_path("/api/v1/menu/{}-callback".format(menu))
+
+    return jsonify({
+        "actions": [
+            {
+                "collect": {
+                    "name": "collect_menu_selection",
+                    "questions": [
+                        {
+                            "question": menu_response,
+                            "name": "menu_selection",
+                            "type": "Twilio.NUMBER"
+                        }
+                    ],
+                    "on_complete": {
+                        "redirect": redirect_path
+                    }
+                }
+            }
+        ]
+    })
+
+
+def build_twilio_collect(question, path):
+    return jsonify({
+        "actions": [
+            {
+                "collect": {
+                    "name": "collect_menu_selection",
+                    "questions": [
+                        {
+                            "question": question,
+                            "name": "menu_selection",
+                            "type": "Twilio.NUMBER"
+                        }
+                    ],
+                    "on_complete": {
+                        "redirect": get_full_api_path(path)
+                    }
+                }
+            }
+        ]
+    })
+
+
+def get_full_api_path(path):
+    return "{}{}".format(API_BASE_PATH, path)
+
+
+def build_twilio_api_redirect(path):
+    return jsonify({"actions": [{"redirect": get_full_api_path(path)}]})
+
+
 def build_twilio_task_redirect(task):
     return jsonify({"actions": [{"redirect": "task://{}".format(task)}]})
 
 
 def build_twilio_say(say_text):
     return jsonify({"actions": [{"say": say_text}]})
-
-# hard coded dinges
 
 
 def get_images_payload():
